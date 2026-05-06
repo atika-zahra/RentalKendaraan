7203d90c1e4e34e253cef5f027ca8ccf0ab5ed65
atika <atika@biztwork.web.id>
2026-05-06 09:22:12 +0700

    membuat menu sewa dan kembali

--- RentalKendaraan/Program.cs
+++ RentalKendaraan/Program.cs
@@ -7,7 +7,78 @@
     new MiniBus("Elf", 100000, "N 5678 E"),
     new MiniBus("HiAce", 445000, "N 6789 F")
 };
+//list itu dinamis, kalau array dia sudah di set dari awal
 
+while (true)
+{
+    Console.WriteLine("--- Rental Kendaraan ---");
+    Console.WriteLine("\n Daftar Kendaraan");
+
+    foreach (var dk in data_kendaraan)
+    {
+        dk.tampilkanInfo();
+    }
+
+    Console.WriteLine("\nPilih menu: ");
+    Console.WriteLine("1. Sewa\n2. Kembali\n3. Keluar");
+    Console.WriteLine("Pilihan Anda: ");
+    string pilihan = Console.ReadLine();
+
+    if (pilihan == "1")
+    {
+        Console.WriteLine(" Input nama kendaraan: ");
+        string nama_kendaraan = Console.ReadLine();
+        var cariKendaraan = data_kendaraan.FirstOrDefault(ck => string.Equals(ck.NamaKendaraan, nama_kendaraan, StringComparison.OrdinalIgnoreCase));
+
+        if (cariKendaraan == null)
+        {
+            Console.WriteLine("\nKendaraan tidak ditemukan");
+        }
+        else if (cariKendaraan.isAvailable)
+        {
+            Console.WriteLine("\nInput jumlah hari sewa: ");
+            int hari = int.Parse(Console.ReadLine());
+
+            double total_sewa = cariKendaraan.HitungTotal(hari);
+
+            cariKendaraan.UbahStatus();
+
+            Console.WriteLine($"Total pembayaran sewa: Rp {total_sewa}");
+        }
+    }
+
+    else if (pilihan == "2")
+    {
+        Console.WriteLine("\n Input nama kendaraan: ");
+        string nama_kendaraan = Console.ReadLine();
+        var cariKendaraan = data_kendaraan.FirstOrDefault(ck => string.Equals(ck.NamaKendaraan, nama_kendaraan, StringComparison.OrdinalIgnoreCase));
+
+        if (cariKendaraan == null)
+        {
+            Console.WriteLine("\nKendaraan tidak ditemukan");
+        }
+        else if (!cariKendaraan.isAvailable)
+        {
+            cariKendaraan.UbahStatus();
+
+            Console.WriteLine("\nKendaraan berhasil dikembalikan!");
+        }
+        else
+        {
+            Console.WriteLine("\n Proses pengembalian tidak bisa dilakukan");
+        }
+    }
+    else if (pilihan == "3")
+    {
+        Console.WriteLine("Tekan ENTER untuk menutup aplikasi...");
+        Console.ReadLine();
+        break;
+    }
+    else
+    {
+        Console.WriteLine("\nTidak valid");
+    }
+}
 class Kendaraan
 {
     //ini field
@@ -15,6 +86,7 @@ class Kendaraan
     protected double _hargaSewaPerHari;
     protected string _nomorPolisi;
     protected bool _isAvailable;
+    //constructor itu set nilai awal dari field 
 
     //ini constructor, ini dipanggil saat membuat object (untuk set nilai awal)
     public Kendaraan(string namaKendaraan, double hargaSewaPerHari, string nomorPolisi)
